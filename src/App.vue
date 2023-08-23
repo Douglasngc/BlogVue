6dfffc4f66564124dc946197a44669e86d4cb1d4
Douglasngc <rbdgoncalves19@gmail.com>
2023-08-23 14:11:23 -0300

    navbar

--- src/App.vue
+++ src/App.vue
@@ -1,16 +1,33 @@
 <template>
+  <navbar/>
   <img alt="Vue logo" src="./assets/logo.png">
-  <HelloWorld msg="Welcome to Your Vue.js App"/>
+  <hr><br>
+  <First msg="Welcome to Your Vue.js App"/>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+import navbar from './components/navbar.vue'
+import First from './components/First.vue'
 
 export default {
   name: 'App',
   components: {
-    HelloWorld
+    First,navbar
   }
 }
 </script>
 
+<style>
+#app {
+  font-family: Avenir, Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+  margin-top: 60px;
+  display:block ;
+ 
+}
+
+
+</style>
